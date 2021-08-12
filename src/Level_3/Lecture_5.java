a0c43a03868138c7aab6962c209eadbe838766db
Karapapl <44815446+Karapapl@users.noreply.github.com>
2021-08-12 16:45:25 +0300

    Update

--- src/Level_3/Lecture_5.java
+++ src/Level_3/Lecture_5.java
@@ -27,3 +27,27 @@ class Quest_5_1 {
   }
 
 }
+
+class Quest_5_2 {
+  public static void main(String [] args) {
+    Scanner console = new Scanner(System.in);
+    System.out.println("Введите первое число:");
+    int number1 = console.nextInt();
+    System.out.println("Введите второе число:");
+    int number2 = console.nextInt();
+    System.out.println("Введите третье число:");
+    int number3 = console.nextInt();
+
+    if ((number1 == number2) && (number2==number3))
+      System.out.println(number1 + " " + number2 + " " + number3);
+    else if (number1 == number2)
+      System.out.println(number1 + " " + number2);
+    else if (number1 == number3)
+      System.out.println(number1 + " " + number3);
+    else if (number2 == number3)
+      System.out.println(number2 + " " + number3);
+    else
+      System.out.println("Введенные числа не равны.");
+    console.close();
+  }
+}
